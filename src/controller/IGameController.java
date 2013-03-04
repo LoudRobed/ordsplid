249658eaa38f1227fe123a5b2fc3cdc2a898b743
Andreas <andhag@stud.ntnu.no>
2013-03-04 12:16:26 +0100

    small fix

--- src/controller/IGameController.java
+++ src/controller/IGameController.java
@@ -1,12 +1,5 @@
 package controller;
-/**
- * Controls the game behaviour. Updates the model and view based on 
- * the game rules.
- * @author Andreas
- *
- */
-public interface IGameController implements  {
 
-	
-	public boolean 
+public interface IGameController {
+
 }
