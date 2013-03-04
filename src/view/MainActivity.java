4cd5e4765f3a85c4ded3630a48478a53bf83703b
Finn <finnjohansen4@gmail.com>
2013-03-04 14:00:24 +0100

    Added buttons to activity_ordsplid.xml Changes to mainactivity

--- src/view/MainActivity.java
+++ src/view/MainActivity.java
@@ -1,14 +1,20 @@
 package view;
 
+import tdt4240.ordsplid.R;
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends Activity  {
 
-	
-	//test, simen
-	public MainActivity() {
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.activity_ordsplid);
+	    
+		Button newGameButton = (Button) findViewById(R.id.new_game_button);
+		Button settingsButton = (Button) findViewById(R.id.settings_button);
+		
+		//newGameButton.setOnClickListener(l)
 
-		// TODO Auto-generated constructor stub
 	}
-
 }
