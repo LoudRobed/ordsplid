c78d03f9e0aaf0d3e5a6f4da98ab4d726c69da64
Simen Owesen-Lein <simen.owesenlein@gmail.com>
2013-03-11 10:47:11 +0100

    s

--- src/model/Letter.java
+++ src/model/Letter.java
@@ -1,5 +1,7 @@
 package model;
 
+import android.graphics.Color;
+
 /**
  * Letter class. Contains information about each letter
  * @author Simen
---
+++ src/system/Input.java
@@ -0,0 +1,5 @@
+package system;
+
+public class Input {
+	public void handleInput() {}
+}
