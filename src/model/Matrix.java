190f693567215e3161671fe8b7ccb6ec04a95572
finnjohansen <finnjohansen4@gmail.com>
2013-03-18 11:17:14 +0100

    - Creater class ButtonAdapter - Changed layout of the game.xml - Creater Matrix class with minor implementation

---
+++ src/model/Matrix.java
@@ -0,0 +1,42 @@
+package model;
+
+public class Matrix implements IMatrix{
+	private static Matrix instance = null;
+	private char[][] m;
+	
+	private Matrix(){
+		
+	}
+	
+	public static Matrix instance() {
+		if (instance == null) instance = new Matrix();
+		return instance;
+	}
+	
+	@Override
+	public char[][] getMatrix() {
+		return m;
+	}
+
+	@Override
+	public void setMatrix(char[][] m) {
+		this.m = m;
+	}
+
+	@Override
+	public void setCharAtIndex(int x, int y, char c) {
+		m[x][y] = c;
+		
+	}
+
+	@Override
+	public int occurencesInMatrix(char c) {
+		for(char[] ch: m){
+			if(ch.equals(c)){
+				return 1;
+			}
+		}
+		return 0;
+	}
+
+}
--- src/view/GameView.java
+++ src/view/GameView.java
@@ -5,22 +5,40 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.GridView;
 
 public class GameView extends Activity{
+	private ButtonAdapter btnAdapter;
+	private String s = "";
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.activity_game_view);
 	    
-	    Button ok = (Button)findViewById(R.id.game_ok_button);
+	    btnAdapter = new ButtonAdapter(this);
+	    
 	    final EditText text = (EditText)findViewById(R.id.resultString);
+	    Button ok = (Button)findViewById(R.id.game_ok_button);
+	    ok.setText("OK");
+	    GridView gridview = (GridView) findViewById(R.id.gridview);
+	    gridview.setAdapter(btnAdapter);
 
-	    ok.setOnClickListener(new OnClickListener() {
+	    btnAdapter.setOnButtonClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				text.setText("ok clicked");
+				// TODO Auto-generated method stub
+				Button temp = (Button) v;
+				s += temp.getText();
+				text.setText(s);
 			}
 		});
+	    
+	    gridview.setOnItemClickListener(new OnItemClickListener() {
+	        public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+	        }
+	    });
 	}
 }
