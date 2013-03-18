3900ca36b2ec6c572574c23f98ab3c643f0d877b
stigvida <stigvida@NTNUIT501.win.ntnu.no>
2013-03-18 13:55:26 +0100

    f

--- src/controller/IGameController.java
+++ src/controller/IGameController.java
@@ -1,5 +1,21 @@
 package controller;
 
-public interface IGameController {
-
+public abstract class IGameController {
+	int time;
+	
+	abstract void newGame();
+	
+	abstract void startGame();
+	
+	abstract void switchPlayer();
+	
+	abstract void updateScore();//PlayerController.updateScore();
+		
+	
+	abstract void endGame(); //launch dialog with scores
+	
+	
+	
+	
+	
 }
--- src/model/IGameRules.java
+++
@@ -1,9 +0,0 @@
-package model;
-/**
- * Defines the rules for a given game.
- * @author Andreas
- *
- */
-public interface IGameRules {
-	
-}
